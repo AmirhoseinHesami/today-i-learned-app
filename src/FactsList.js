caf9a65828ffe9a31e349ff1ecd41e7eb811f18c
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-03 13:23:59 +0330

    implement the facts list section with its functionality and logic

---
+++ src/FactsList.js
@@ -0,0 +1,33 @@
+import Fact from "./Fact";
+
+function FactsList({ facts, categories, setFacts }) {
+  if (!facts.length) {
+    return (
+      <p className="message">
+        No facts for this category yet! Create the first one ✌️
+      </p>
+    );
+  }
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact
+            fact={fact}
+            setFacts={setFacts}
+            key={fact.id}
+            categories={categories}
+          />
+        ))}
+      </ul>
+      <p className="text-center">
+        {facts.length === 1
+          ? "There is 1 fact in the database. Add your own!"
+          : `There are ${facts.length} facts in the database. Add your own!`}
+      </p>
+    </section>
+  );
+}
+
+export default FactsList;
