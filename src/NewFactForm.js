536d737cd2c1351bd788d1d9f50700ae04f49984
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-03 13:26:13 +0330

    the new fact form component with the functionality to validate the input and then add the new fact to the dom

---
+++ src/NewFactForm.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import supabase from "./supabase";
+
+function isValidHttpUrl(string) {
+  try {
+    const newUrl = new URL(string);
+    return newUrl.protocol === "http:" || newUrl.protocol === "https:";
+  } catch (err) {
+    return false;
+  }
+}
+
+function NewFactForm({
+  categories,
+  setShowForm,
+  setFacts,
+  setCurrentCategory,
+  currentCategory,
+}) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const textLength = text.length;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (text && category && textLength <= 200 && isValidHttpUrl(source)) {
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from("facts")
+        .insert([{ text, source, category }])
+        .select();
+
+      setIsUploading(false);
+      if (currentCategory !== "all") setCurrentCategory(category);
+
+      if (!error) setFacts((facts) => [newFact[0], ...facts]);
+      else alert("There was a problem uploading Data.");
+
+      setText("");
+      setSource("");
+      setCategory("");
+
+      setShowForm(false);
+    }
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        disabled={isUploading}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+        disabled={isUploading}
+      />
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        disabled={isUploading}
+      >
+        <option value="">Choose category:</option>
+        {categories.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large" disabled={isUploading}>
+        Post
+      </button>
+    </form>
+  );
+}
+
+export default NewFactForm;
